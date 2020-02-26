e5b65aeb571195dd1a9dadc5a30a9b9def322426
Andre Camargo <camargo_and@hotmail.com>
2020-02-26 17:17:03 -0300

    calculadora versao final

--- Calculadora.java
+++ Calculadora.java
@@ -10,40 +10,48 @@ public class Calculadora{
 
         int resultado = 0;
 
-        primeiroValor = Integer.parseInt(JOptionPane.showInputDialog("Insira numero Inteiro:"));
+        String sair = "a";
 
         do{
-            operador = JOptionPane.showInputDialog("Selecione:\n+ (somar)\n - (subtrair)\n * (multiplicar)\n / (dividir)");
 
-        }while(!operador.equals("+") && !operador.equals("-") && !operador.equals("*") && !operador.equals("/"));
+            primeiroValor = Integer.parseInt(JOptionPane.showInputDialog("Insira numero Inteiro:"));
 
-        segundoValor = Integer.parseInt(JOptionPane.showInputDialog("Insira numero Inteiro:"));
+            do{
+                operador = JOptionPane.showInputDialog("Selecione:\n+ (somar)\n - (subtrair)\n * (multiplicar)\n / (dividir)");
+
+            }while(!operador.equals("+") && !operador.equals("-") && !operador.equals("*") && !operador.equals("/"));
+
+            segundoValor = Integer.parseInt(JOptionPane.showInputDialog("Insira numero Inteiro:"));
         
-        if(operador.equals("+")){
+            if(operador.equals("+")){
 
-            resultado = primeiroValor + segundoValor;
+                resultado = primeiroValor + segundoValor;
 
-        }else if (operador.equals("-")){
+            }else if (operador.equals("-")){
 
-            resultado = primeiroValor - segundoValor;
+                resultado = primeiroValor - segundoValor;
 
-        }else if (operador.equals("*")){
+            }else if (operador.equals("*")){
 
-            resultado = primeiroValor * segundoValor;
+                resultado = primeiroValor * segundoValor;
 
-        }else if (operador.equals("/")){
-            if(segundoValor == 0){
+            }else if (operador.equals("/")){
+                if(segundoValor == 0){
 
-                JOptionPane.showMessageDialog(null, "Não é possivel fazer a divisão por Zero");
+                    JOptionPane.showMessageDialog(null, "Não é possivel fazer a divisão por Zero");
                 
-                System.exit(0);
+                }else{
+                    resultado = primeiroValor / segundoValor;
+                }
             }
-            resultado = primeiroValor / segundoValor;
-        }
 
+            if(segundoValor != 0){
 
-        JOptionPane.showMessageDialog(null, primeiroValor + operador + segundoValor + " = " + resultado);
-        
+                JOptionPane.showMessageDialog(null, primeiroValor + operador + segundoValor + " = " + resultado);
+            }
+        sair = JOptionPane.showInputDialog("Deseja continuar:\n s para Sair");
+
+        }while(!sair.equals("s"));
 
     }
 
